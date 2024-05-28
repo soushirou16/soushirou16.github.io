ff9ed795ef0979d4717ff2b9d1f6e25e0904e8ad
Nick Li <linickosu@gmail.com>
2024-05-28 14:31:21 -0400

    edits

--- static/js/strava.js
+++ static/js/strava.js
@@ -7,6 +7,8 @@ document.getElementById('authbutton').addEventListener('click', () => {
 
 
 var datadict = {}
+var totalMiles = 0;
+var totalElapsedTime = 0;
 // logic for displaying data. sends a call to api
 document.getElementById('displaybutton').addEventListener('click', function() {
     fetch('/athlete/activities')
@@ -21,16 +23,19 @@ document.getElementById('displaybutton').addEventListener('click', function() {
 
         // color the squares
         // only checking by activity. make it so it's by day and store the values to be used later.
+
+        let todaysDate = new Date();
+        let todaysKey = convertToKey(todaysDate);
+        
+        let todaysDiv = document.getElementById(todaysKey);
+        todaysDiv.style.borderColor = "#7E1313";
+        
         
         data.forEach(activity => { 
-            var startDate = new Date(activity.start_date);
-
-            var month = (startDate.getMonth() + 1).toString(); 
-            var day = startDate.getDate().toString();
-            var date = month + "/" + day;
-            var key = "M" + month + "_D" + day;
+            let date = new Date(activity.start_date);
+            let key = convertToKey(date);
             
-            var currDay = document.getElementById(key);
+            let currDay = document.getElementById(key);
             
             if (!datadict[key]){
                 datadict[key] = [];
@@ -43,17 +48,28 @@ document.getElementById('displaybutton').addEventListener('click', function() {
                 'elapsed_time': activity.elapsed_time,
                 'start_time': activity.start_date_local
             });
+
+            totalMiles += activity.distance;
+            totalElapsedTime += activity.elapsed_time;
             
             if(activity.distance < 1800){
                 currDay.classList.add('lvl1green');
             }
-            else if (activity.distance < 3400){
+            else if (activity.distance < 3600){
                 currDay.classList.add('lvl2green');
             }
-            else if (activity.distance < 5000){
+            else if (activity.distance < 5400){
                 currDay.classList.add('lvl3green');
             }
+            else{
+                currDay.classList.add('lvl4green');
+            }
         });        
+        document.getElementById('totalmilestext').textContent += (totalMiles/1600).toFixed(2) + " miles";
+
+        var formattedTime = formatTime(totalElapsedTime);
+        document.getElementById('totaltimetext').textContent += formattedTime;
+
     })
 });
 
@@ -64,25 +80,31 @@ var childDivs = moreInfoDiv.querySelectorAll("p");
 var timeout;
 
 function handleDayHover(event) {
-    var currentDay = this;
-    var currentDayID = currentDay.id;
+    let currentDay = this;
+    let currentDayID = currentDay.id;
     
     if (datadict[currentDayID] && datadict[currentDayID].length > 0) {
-        var cursorX = event.clientX;
-        var cursorY = event.clientY;
+        let cursorX = event.clientX;
+        let cursorY = event.clientY;
 
         moreInfoDiv.style.left = cursorX + 40 + 'px';
         moreInfoDiv.style.top = cursorY + + -70 + 'px';
 
+        let elapsedTimeInMinutes = datadict[currentDayID][0]['elapsed_time'] / 60;
+        let minutes = Math.floor(elapsedTimeInMinutes);
+        let seconds = Math.floor((elapsedTimeInMinutes - minutes) * 60);
+        let formattedSeconds = (seconds < 10 ? '0' : '') + seconds;
+        let formattedTime = minutes + ':' + formattedSeconds + " min";
+
         childDivs[0].textContent = datadict[currentDayID][0]['date'];
-        childDivs[1].textContent = (datadict[currentDayID][0]['distance'] / 1600).toFixed(2) + " miles";
-        childDivs[2].textContent = (datadict[currentDayID][0]['elapsed_time'] / 60).toFixed(2) + "min";
+        childDivs[1].textContent = (datadict[currentDayID][0]['distance'] / 1600).toFixed(2) + " miles"+ "\t" + "(" + formattedTime + ")";
+        childDivs[2].textContent;
 
         timeout = setTimeout(function(){
         moreInfoDiv.style.opacity = '100';
         }, 350);
     } else {
-        moreInfoDiv.style.display = '0';
+        moreInfoDiv.style.opacity = '0';
     }
 }
 
@@ -98,4 +120,25 @@ for (var i = 0; i < days.length; i++) {
         moreInfoDiv.style.opacity = '0';
         clearTimeout(timeout);
     });
+}
+
+
+function convertToKey(startDate){
+    let month = (startDate.getMonth() + 1).toString(); 
+    let day = startDate.getDate().toString();
+    let key = "M" + month + "_D" + day;
+    return key;
+}
+
+function formatTime(seconds) {
+    var hours = Math.floor(seconds / 3600);
+    var minutes = Math.floor((seconds % 3600) / 60);
+    var remainingSeconds = seconds % 60;
+
+    // Add leading zeros if necessary
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    remainingSeconds = (remainingSeconds < 10) ? '0' + remainingSeconds : remainingSeconds;
+
+    return hours + ':' + minutes + ':' + remainingSeconds;
 }
